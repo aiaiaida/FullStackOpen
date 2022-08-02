b541c65c2f3b2d8d9578f949907113b4f80da583
aada <aada@aadadeMacBook-Pro.local>
2022-08-02 14:36:50 +0300

    1.6 completed

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -1,50 +1,23 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
+  const increasebyone = () => {setCounter(counter+1)}
+  const decreasebyone = () => {setCounter(counter - 1)}
+  const zero = () => {setCounter(0)}
 
-  const Header = (props) => {
-    return (<h1>{course.name}</h1>)
-  }
+  const Display = ({counter}) => <div>{counter}</div>
 
-  const Content = (props) => {
-    return (
-      <div>
-        <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-        <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
-        <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-      <div>
-        <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-      </div>
-    )
-  }
+  const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content course={course}></Content>
-      <Total course={course}></Total>
+      <Display counter={counter}></Display>
+      <Button onClick={increasebyone} text='plus'></Button>
+      <Button onClick={decreasebyone} text='minus'></Button>
+      <Button onClick={zero} text='zero'></Button>
     </div>
+    
   )
 }
 

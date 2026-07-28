efcb98695c70255a499da5e78b1387d2ef2e8879
aada <aadaappelsiini@gmail.com>
2026-07-28 09:28:13 +0300

    3.11 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -10,6 +11,7 @@ morgan.token('body', req => {
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms :body'))
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')))
 
 let persons = [
     { 

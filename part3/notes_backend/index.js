4c20ae6288e6dfec659a2735a67cd2f9c20f69ed
aada <aadaappelsiini@gmail.com>
2026-07-29 14:42:26 +0300

    notes mongoDB integration

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,43 +1,29 @@
+require('dotenv').config()
 const express = require('express')
+const note = require('./models/note')
 const app = express()
+const Note = require(('./models/note'))
 
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+let notes = []
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request,response) => {
     const id = request.params.id
-    const note = notes.find(note=>note.id === id)
-    if (note) {
+    Note.findById(id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -54,24 +40,24 @@ const generateID = () => {
 
 app.post('/api/notes/', (request,response) => {
     const body = request.body
+    
     if (!body.content) {
         return response.status(400).json({
             error: "content missing"
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateID()
-    }
+        })
 
-    notes = notes.concat(note)
-    console.log(notes)
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })

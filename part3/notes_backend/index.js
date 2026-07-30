0bdcb5c8254d8ea4e854763ba4644d6a0244527f
aada <aadaappelsiini@gmail.com>
2026-07-30 08:00:01 +0300

    notes more operations DB

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -9,48 +9,78 @@ app.use(express.static('dist'))
 
 let notes = []
 
+// info
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+// all notes
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (request,response) => {
+// one note by id
+app.get('/api/notes/:id', (request,response, next) => {
     const id = request.params.id
     Note.findById(id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => {next(error)})
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+// delete one note by id
+app.delete('/api/notes/:id', (request, response, next) => {
     const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-app.post('/api/notes/', (request,response) => {
-    const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing"
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        })
-
-    note.save().then(savedNote => {
-        response.json(savedNote)
+    Note.findByIdAndDelete(id).then(res => {
+        response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
+// update a note
+app.put('api/note/:id', (request, response, next) => {
+    const {content, important} = request.body
+
+    Note.findById(request.params.id).then(returnedNote => {
+        if (!returnedNote) {
+            return response.status(404).end()
+        }
+
+        returnedNote.content = content
+        returnedNote.important = important
+
+        return returnedNote.save().then(updated => {
+            response.json(updated)
+        })
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+// error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

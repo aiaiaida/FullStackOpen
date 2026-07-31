040ec5e1f69dc8140f3b808f79cb80fa2b9f7cb9
aada <aadaappelsiini@gmail.com>
2026-07-31 07:20:28 +0300

    3.19 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -53,7 +53,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
 })
 
 // add a new person
-app.post("/api/persons/", (request, response) => {
+app.post("/api/persons/", (request, response, next) => {
     const body = request.body
     if (!body.name) {
         return response.status(400).json({
@@ -71,6 +71,7 @@ app.post("/api/persons/", (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 // update an existing
@@ -96,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError'){
         return response.status(400).send({error:'malformatted ID'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
     next(error)
 }

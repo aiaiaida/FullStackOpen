dada5713bccf77f573c9d40746b130f26456fa65
aada <aadaappelsiini@gmail.com>
2026-08-03 19:41:22 +0300

    4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,4 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  } else if (blogs.length === 1) {
+    return blogs[0]
+  } else {
+    return blogs.reduce((fav, blog) => {
+      return fav.likes >= blog.likes ? fav : blog}, blogs[0])}
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog}

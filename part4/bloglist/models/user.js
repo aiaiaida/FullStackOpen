08dee8c6f2867e1aabd2fb39d6546090933f3b22
aada <aadaappelsiini@gmail.com>
2026-08-17 20:47:12 +0300

    4.15 done

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+// schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  passwordHash: String,
+  name: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User

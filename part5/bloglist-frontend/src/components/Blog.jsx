33bc2fde4b79613c5a8c109de3ed15e46835c08d
aada <aadaappelsiini@gmail.com>
2026-08-20 10:57:49 +0300

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailVisible, setDetailVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {!detailVisible && (
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setDetailVisible(!detailVisible)}>view</button> 
+        </div>
+      )}
+      {detailVisible && (
+        <div>
+          <div>
+            {blog.title}
+            <button onClick={() => setDetailVisible(!detailVisible)}>hide</button>
+          </div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog

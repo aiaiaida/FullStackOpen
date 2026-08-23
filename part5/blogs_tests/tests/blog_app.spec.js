d82b8e69e4eaa83ca1b77b80dc31d66c36742da2
aada <aadaappelsiini@gmail.com>
2026-08-23 15:24:42 +0300

    5.19 done

--- part5/blogs_tests/tests/blog_app.spec.js
+++ part5/blogs_tests/tests/blog_app.spec.js
@@ -25,7 +25,7 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await loginWith(page, 'aada', 'wrong')
+      await loginWith(page, 'aada', 'salainen')
       await expect(page.getByText('logged in')).toBeVisible()
     })
     
@@ -33,6 +33,26 @@ describe('Blog app', () => {
       await loginWith(page, 'aada', 'wrong')
       const errorDiv = page.locator('.error')
       await expect(errorDiv).toContainText('wrong credentials')
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page,'aada','salainen')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'create a new blog' }).click()
+      await page.getByLabel('title').fill('a note by playwright')
+      await page.getByLabel('author').fill('playwright')
+      await page.getByLabel('url').fill('http:playwright')
+      await page.getByRole('button', { name: 'create' }).click()
+
+      const infoDiv = page.locator('.info')
+      await expect(infoDiv).toContainText('a note by playwright')
+
+      const blog = page.getByRole('button', { name: 'view' }).locator('..')
+      await expect(blog).toContainText('a note by playwright')
     })
   })
 })
